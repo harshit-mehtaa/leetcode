77f8f1f8d65ab1e0030391749cc13d69a03f043e
Harshit Mehta <hdm23061993@gmail.com>
2020-06-15 21:12:10 +0530

    Update june15.py

--- june-daily-challenge-2020/june15.py
+++ june-daily-challenge-2020/june15.py
@@ -1,7 +1,9 @@
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        if root is None: return None
-        elif root.val == val: return root
-        else:
-            if val < root.val: return self.searchBST(root.left, val)
-            if val > root.val:  return self.searchBST(root.right, val)
+        if not root: 
+            return None
+        if root.val == val: 
+            return root
+        if val < root.val: 
+            return self.searchBST(root.left, val)
+        return self.searchBST(root.right, val)
